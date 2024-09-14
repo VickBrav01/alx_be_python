8b1ee54b2e29c3ab4af14361e72a3b9516a06846
Vbrav01 <victortosh001@gmail.com>
2024-09-14 11:58:05 +0300

    Arithmetic operations

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,9 @@
+number1 = 10
+number2 = 5
+
+sum = number1 + number2 
+print("Addition of ", number1, " and ", number2 ,"is ", sum) 
+subtraction = number1 - number2 
+print("Subtraction of ", number1, " and ", number2 ,"is ", subtraction) 
+multiplication = number1 * number2 
+print("Multiplication of ", number1, " and ", number2 ,"is ", multiplication) 
