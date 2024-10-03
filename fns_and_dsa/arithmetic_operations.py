f34b59f1eda75c47606723ed0fc4a9f27d545a15
Vbrav01 <victortosh001@gmail.com>
2024-10-03 10:39:58 +0300

    arithmetic_operations.py

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -7,7 +7,9 @@ def perform_operation(num1, num2, operation):
         case "multiply":
             return num1 * num2
         case "divide":
-            if (num1 and num2 ) == 0:
+            if num1 == 0:
+                return f"Cannot divide a number with zero"
+            elif num2 == 0:
                 return f"Cannot divide a number with zero"
             else:
                 return num1 / num2
